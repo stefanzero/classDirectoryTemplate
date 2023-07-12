a05d64c5be692d5fae40013ff73aa635c29ab88f
Glitch (glitch-blank-website) <none>
2023-07-12 17:36:20 +0000

    💘🏃 Checkpoint ./index.html:59800714/470 ./style.css:59800714/538 ./script.js:59800714/428

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
-
-console.log("Hello, world!");
+function setDate() {
+  let dateViews = document.getElementsByClassName("date-view");
+  
+  let currentDate = Date();
+  
+  for (var index = 0; index < dateViews.length; index++) {
+    dateViews[index].innerHTML = currentDate;
+  }
+}
