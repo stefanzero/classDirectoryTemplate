6207bc5bedbe23f70a6985ae572e989ff6fefd62
Glitch (glitch-blank-website) <none>
2023-07-18 22:07:27 +0000

    📫🍽 Checkpoint ./index.html:59800714/139 ./script.js:59800714/230 ./style.css:59800714/255

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
-function setDate() {
-  let dateViews = document.getElementsByClassName("date-view");
-  
-  let currentDate = Date();
-  
-  for (var index = 0; index < dateViews.length; index++) {
-    dateViews[index].innerHTML = currentDate;
-  }
-}
