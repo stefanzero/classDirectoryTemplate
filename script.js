66b1861345e467cffafc21120a2152c1149e9ccf
Glitch (glitch-blank-website) <none>
2023-08-08 16:59:13 +0000

    💗🐝 Checkpoint ./index.html:59214970/62435 ./script.js:59214970/5337 ./style.css:59214970/2535 ./testamonials.css:59214970/54309 ./search.css:59214970/1212 ./hamburger.css:59214970/1487 ./estyle.css:59214970/1487 ./darkmode.css:59214970/1303 ./cards.css:59214970/97434 ./carasoule.css:59214970/6423 ./.glitch-assets:59214970/384

--- script.js
+++ script.js
@@ -1,37 +1,130 @@
-/* This will be our code to fetch data from Airtable to our Glitch. 
-  Missing code to LOOP over the DATA and APPEND them in the students profile card -> try using querySelectorAll(.<whatever the class name enter here>)
-  PLEASE DO NOT CHANGE WITHOUT CONSULTING --> INSTRUCTOR
-*/
+
+/*
+const container = document.getElementById("container");
+
 const getAirtableData = async () => {
+  const apiKey = "patwSsvy7Hvo5mSGS.9799e293a34039bfd554077a19779c0bc8b47f161fcda595acaa76f6b26ee9a4";
+  const baseId = "appPABgKKuNzeb4l6";
+  const tableName = "Trainees";
+
+  const url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
   const options = {
     method: "GET",
     headers: {
-      Authorization: `Bearer patwSsvy7Hvo5mSGS.9799e293a34039bfd554077a19779c0bc8b47f161fcda595acaa76f6b26ee9a4`,
+      Authorization: `Bearer ${apiKey}`,
     },
   };
-  try {
-    const response = await fetch(
-      `https://api.airtable.com/v0/appPABgKKuNzeb4l6/Trainees`,
-      options
-    );
-    const data = await response.json();
-    console.log(data);
-    for (var i = 0; i < data.records.length; i++) {
-      console.log(`${data.records[i].fields.name} --> ${data.records[i].fields.email} --> ${data.records[i].fields.linkedIn}`);  
 
-    
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from Airtable.");
     }
+
+    const data = await response.json();
+
+    // Sort the records alphabetically by name before processing
+    data.records.sort((a, b) => a.fields.name.localeCompare(b.fields.name));
+
+    let content = "";
+    for (const record of data.records) {
+      const name = record.fields.name || "Name not available";
+      const email = record.fields.email || "Email not available";
+      const linkedIn = record.fields.linkedIn || "#";
+      const headshot = record.fields.headshot;
+
+      // Check if the trainee has a picture (headshot) and set the URL accordingly
+      const headshotUrl = headshot ? headshot[0]?.url : "https://example.com/default-avatar.jpg";
+
+      content += `
+        <div>
+          <img src="${headshotUrl}" />
+          <h1>Name: ${name}</h1>
+          <p>Email: ${email}</p>
+          <p><a href="${linkedIn}" target="_blank">LinkedIn</a></p>
+        </div>
+      `;
+    }
+
+    container.innerHTML = content;
   } catch (error) {
-    console.log(error);
+    console.log("Error:", error);
+    container.innerHTML = "<p>Failed to fetch data. Please try again later.</p>";
   }
 };
 
 getAirtableData();
+*/
+
+
+/* sidebar */
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+// Call the resetMenu function when the window resizes
+window.addEventListener("resize", resetMenu);
+
+function resetMenu() {
+  if (window.innerWidth >= 900) {
+    document.getElementById("menu-bar").style.display = 'flex';
+  } else {
+    document.getElementById("menu-bar").style.display = 'none';
+  }
+}
 
 
 
-/* Function to change theme {Dark | Light}. TIPS -> classList.Toggle()*/
 
-/* Function to dynamically change the year date of the footer. TIPS use the date built in function*/
 
-/* Function to loop over students testimonials - footer. TIPS .length*/
+/*Toggle dark mode */
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+
+document.body.classList.toggle("dark-mode", prefersDarkMode);
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+darkModeToggle.addEventListener("click", toggleDarkMode);
+
+
+
+/*Cards option*/
+$(function () {
+  $(".material-card > .mc-btn-action").click(function () {
+    var card = $(this).parent(".material-card");
+    var icon = $(this).children("i");
+    icon.addClass("fa-spin-fast");
+
+    if (card.hasClass("mc-active")) {
+      card.removeClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-arrow-left")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-bars");
+      }, 800);
+    } else {
+      card.addClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-bars")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-arrow-left");
+      }, 800);
+    }
+  });
+});
+
+
+
+/*Search option */
